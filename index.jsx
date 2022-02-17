8e709e224482153d456899e9d46600f4b3ffe0e8
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-02-17 21:58:10 +0100

    first element rendered, more parcel errors

---
+++ index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <h1>My Wishlist</h1>,//elemento que insertamos
+    document.getElementById("root")//onde o insertamos
+);
