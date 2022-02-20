5223c8e68981196ce17a21a07461f4eeac97d518
diego-rg <76407123+diego-rg@users.noreply.github.com>
2022-02-20 20:43:53 +0100

    add wishes logic

--- App.jsx
+++ App.jsx
@@ -1,24 +1,27 @@
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable semi */
 /* eslint-disable quotes */
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import WishInput from "./WishInput";// Importamos o compoñente que representa o input de texto do desexo
 import WishList from "./WishList";// Importamos o compoñente que representa a lista de desexos
 
-const wishes = [
+const initialWishes = [
   { description: "Become a Web developer", done: false },
   { description: "Buy a car", done: true }
 ];
 
 // BEM: ex: bloque: wish-input, elemento: wish-input__label wish-input__field. Con subelementos podemos usar: wish-input__label span
-const App = () => (
-  <div className="app">
+const App = () => {
+  const [wishes, setWishes] = useState(initialWishes);
+  return (
+    <div className="app">
     <h1>My wishlist</h1>
-    <WishInput />
+    <WishInput onNewWish={wish => setWishes([wish, ...wishes])} />
     <WishList wishes={wishes}/>
     <button type="button" className="wish-clear">Archive all done</button>
   </div>
-);
+  );
+}
 
 export default App;
--- WishInput/WishInput.jsx
+++ WishInput/WishInput.jsx
@@ -1,13 +1,31 @@
 /* eslint-disable semi */
 /* eslint-disable quotes */
-import React from "react";
+import React, { useState } from "react";
+import PropTypes from 'prop-types';
 
 // compoñente que representa o input de texto do desexo
-const WishInput = () => (
+const WishInput = ({ onNewWish }) => {
+  const [newWishText, setNewWishText] = useState("");
+  return (
     <fieldset className="wish-input">
       <legend className="wish-input__label">New wish:</legend>
-      <input className="wish-input__field" type="text" placeholder="I want to..." />
+      <input className="wish-input__field" type="text" placeholder="I want to..." value={newWishText} onChange={e => setNewWishText(e.target.value)}
+      onKeyUp={(e) => { // Mellor un btn??
+        if (e.key === "Enter" && newWishText.length) {
+          onNewWish({ done: false, description: newWishText });
+          setNewWishText("") // limpa o input despois de mandalo
+        }
+      }} />
     </fieldset>
-);
+  );
+}
+
+WishInput.propTypes = {
+  onNewWish: PropTypes.func
+}
+
+WishInput.defaultProps = {
+  onNewWish: () => {}
+}
 
 export default WishInput;
